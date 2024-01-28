2aad77bf11220531d82e9ba5d11302f5cef6b0f3
PC <gustavosbocato@gmail.com>
2024-01-27 22:28:17 -0300

    adicionei varias parada

--- js-curso-2-aula1/js-curso-2-aula_-/app.js
+++ js-curso-2-aula1/js-curso-2-aula_-/app.js
@@ -1,7 +1,7 @@
-let numeroSecreto = gerarNaturalAleatório(1, 10);
+let numeroSecreto;
+let numeroDeTentativas;
 
-exibirTextoNaTela('h1', 'Jogo de advinhação de um número secreto.');
-exibirTextoNaTela('p', 'Entre um número natural de 1 a 100');
+novoJogo();
 
 function exibirTextoNaTela(tag, texto){
 
@@ -13,7 +13,32 @@ function exibirTextoNaTela(tag, texto){
 function verificarChute() {
 
     let chute = document.querySelector('input').value;
-    console.log(numeroSecreto == chute);
+    numeroDeTentativas++;
+    
+    if(chute == numeroSecreto){
+
+        let palavraTentativa = numeroDeTentativas > 1 ? 'tentativas' : 'tentativa';
+        exibirTextoNaTela('h1', `Você advinhou o número! Em ${numeroDeTentativas} ${palavraTentativa}.`);
+        exibirTextoNaTela('p', 'Parabéins sindi!');
+
+        document.getElementById('reiniciar').removeAttribute('disabled');
+
+    } else{
+
+        exibirTextoNaTela('h1', 'Você errou o número :(');
+        document.querySelector('input').value = '';
+
+        if(chute > numeroSecreto){
+
+            exibirTextoNaTela('p', 'Tente um número menor.');
+
+        }else{
+
+            exibirTextoNaTela('p', 'Tente um número maior.');
+
+        }
+
+    }
 
 }
 
@@ -28,5 +53,18 @@ function gerarNaturalAleatório(minN, maxN){
     }
 
     return numeroAleatorio;
+
+}
+
+function novoJogo(){
+
+    numeroSecreto = gerarNaturalAleatório(1, 10);
+    numeroDeTentativas = 0;
+
+    exibirTextoNaTela('h1', 'Jogo de advinhação de um número secreto.');
+    exibirTextoNaTela('p', 'Entre um número natural de 1 a 10');
+
+    document.querySelector('input').value = '';
+    document.getElementById('reiniciar').setAttribute('disabled', 'true');
 
 }
